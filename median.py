112c02d8671677531bb0325d9e31f2c41738ae91
Mahadevan007 <42490734+Mahadevan007@users.noreply.github.com>
2019-07-18 11:06:36 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,7 +1,7 @@
 n = int(input())
 mylist = list(map(int,input().split()))
 mylist = sorted(mylist)
-if n%2==0:
+if n%2!=0:
     print(mylist[int(n/2)])
 else:
     print(mylist[int(n-1/2)]+mylist[int(n/2)]/2)
