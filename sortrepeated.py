568df066e66260a558e26af39392099c0a14a655
Mahadevan007 <42490734+Mahadevan007@users.noreply.github.com>
2019-07-19 12:35:01 +0530

    Create sortrepeated.py

---
+++ sortrepeated.py
@@ -0,0 +1,15 @@
+n = int(input())
+mylist = list(map(int,input().split()))
+mydict = {}
+for i in mylist:
+    if i in mydict:
+        mydict[i] += 1
+    else:
+        mydict[i] = 1
+print(mydict)
+anslist = []
+for i in mydict:
+    if mydict[i]>1:
+        anslist.append(i)
+anslist = sorted(anslist)
+print(*anslist)
